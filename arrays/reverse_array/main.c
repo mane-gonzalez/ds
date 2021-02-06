17acb8167aad50fce976111a9b7f33c0088ad77b
Manuel <gonzalez.manuel@live.com>
2021-02-05 21:40:31 -0600

    https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/

---
+++ arrays/reverse_array/main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+void reverse_array(int arr[], int size);
+void reverse_array_recursion(int arr[], int start, int end);
+
+void print_array(int arr[], int size);
+
+int main(int argc, char **argv)
+{
+	int array[] = {1,2,3,4,5,6,7,8,9,10};
+    
+    int size = sizeof(array)/sizeof(array[0]);
+    
+    reverse_array_recursion(array, 0, size-1);
+    
+    print_array(array,size);
+    
+	return 0;
+}
+
+void reverse_array(int arr[], int size)
+{
+    int start = 0;
+    int end = size -1;
+    int temp = 0;
+    
+    while(start < end)
+    {
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;            
+    }
+    
+}
+
+void reverse_array_recursion(int arr[], int start, int end)
+{
+
+    int temp = 0;
+    
+    if (start >= end)
+        return;
+        
+    temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;            
+    
+    reverse_array_recursion(arr, start +1, end -1);
+}
+
+void print_array(int arr[],int size)
+{
+    for(int i=0;i<size;i++)
+    {
+        printf("-%d-", arr[i]);
+    }
+}
---
+++ arrays/reverse_array/reverse_array.mk
@@ -0,0 +1,108 @@
+##
+## Auto Generated makefile by CodeLite IDE
+## any manual changes will be erased      
+##
+## Debug
+ProjectName            :=reverse_array
+ConfigurationName      :=Debug
+WorkspacePath          :=/home/m/geekforgeeks/ds/arrays
+ProjectPath            :=/home/m/geekforgeeks/ds/arrays/reverse_array
+IntermediateDirectory  :=$(ConfigurationName)
+OutDir                 := $(IntermediateDirectory)
+CurrentFileName        :=
+CurrentFilePath        :=
+CurrentFileFullPath    :=
+User                   :=m
+Date                   :=05/02/21
+CodeLitePath           :=/home/m/.codelite
+LinkerName             :=/bin/clang++-10
+SharedObjectLinkerName :=/bin/clang++-10 -shared -fPIC
+ObjectSuffix           :=.o
+DependSuffix           :=.o.d
+PreprocessSuffix       :=.i
+DebugSwitch            :=-g 
+IncludeSwitch          :=-I
+LibrarySwitch          :=-l
+OutputSwitch           :=-o 
+LibraryPathSwitch      :=-L
+PreprocessorSwitch     :=-D
+SourceSwitch           :=-c 
+OutputFile             :=$(IntermediateDirectory)/$(ProjectName)
+Preprocessors          :=
+ObjectSwitch           :=-o 
+ArchiveOutputSwitch    := 
+PreprocessOnlySwitch   :=-E
+ObjectsFileList        :="reverse_array.txt"
+PCHCompileFlags        :=
+MakeDirCommand         :=mkdir -p
+LinkOptions            :=  
+IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
+IncludePCH             := 
+RcIncludePath          := 
+Libs                   := 
+ArLibs                 :=  
+LibPath                := $(LibraryPathSwitch). 
+
+##
+## Common variables
+## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
+##
+AR       := /bin/llvm-ar-10 rcu
+CXX      := /bin/clang++-10
+CC       := /bin/clang-10
+CXXFLAGS :=  -g -O0 -Wall $(Preprocessors)
+CFLAGS   :=  -g -O0 -Wall $(Preprocessors)
+ASFLAGS  := 
+AS       := /bin/llvm-as-10
+
+
+##
+## User defined environment variables
+##
+CodeLiteDir:=/usr/share/codelite
+Objects0=$(IntermediateDirectory)/main.c$(ObjectSuffix) 
+
+
+
+Objects=$(Objects0) 
+
+##
+## Main Build Targets 
+##
+.PHONY: all clean PreBuild PrePreBuild PostBuild MakeIntermediateDirs
+all: $(OutputFile)
+
+$(OutputFile): $(IntermediateDirectory)/.d $(Objects) 
+	@$(MakeDirCommand) $(@D)
+	@echo "" > $(IntermediateDirectory)/.d
+	@echo $(Objects0)  > $(ObjectsFileList)
+	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
+
+MakeIntermediateDirs:
+	@test -d $(ConfigurationName) || $(MakeDirCommand) $(ConfigurationName)
+
+
+$(IntermediateDirectory)/.d:
+	@test -d $(ConfigurationName) || $(MakeDirCommand) $(ConfigurationName)
+
+PreBuild:
+
+
+##
+## Objects
+##
+$(IntermediateDirectory)/main.c$(ObjectSuffix): main.c
+	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/main.c$(ObjectSuffix) -MF$(IntermediateDirectory)/main.c$(DependSuffix) -MM main.c
+	$(CC) $(SourceSwitch) "/home/m/geekforgeeks/ds/arrays/reverse_array/main.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.c$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/main.c$(PreprocessSuffix): main.c
+	$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.c$(PreprocessSuffix) main.c
+
+
+-include $(IntermediateDirectory)/*$(DependSuffix)
+##
+## Clean
+##
+clean:
+	$(RM) -r $(ConfigurationName)/
+
+
